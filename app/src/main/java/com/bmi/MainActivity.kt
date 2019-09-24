b109a921a95fc236fd67a717c69c070d759d7ffb
AvanNorth <avannorthy@users.noreply.github.com>
2019-09-24 18:36:09 +0300

    ы

--- app/src/main/java/com/bmi/MainActivity.kt
+++ app/src/main/java/com/bmi/MainActivity.kt
@@ -11,20 +11,37 @@ import android.widget.NumberPicker
 class MainActivity : AppCompatActivity() {
 
     lateinit var toolbar:Toolbar
-    lateinit var picker:NumberPicker
+    lateinit var weightPicker:NumberPicker
+    lateinit var heightPicker:NumberPicker
+    lateinit var genderPicker:NumberPicker
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
 
-        picker = findViewById(R.id.number_picker)
+        weightPicker = findViewById(R.id.weight_picker)
+        heightPicker = findViewById(R.id.height_picker)
+        genderPicker = findViewById(R.id.gender_picker)
 
-        val data = arrayOf("Berlin", "Moscow", "Tokyo")
+        weightPicker.wrapSelectorWheel = false
+        heightPicker.wrapSelectorWheel = false
+        genderPicker.wrapSelectorWheel = false
 
-        picker.setMinValue(0)
-        picker.setMaxValue(data.size - 1)
-        picker.setDisplayedValues(data)
+
+        weightPicker.minValue = 20
+        weightPicker.maxValue = 200
+
+        heightPicker.minValue = 50
+        heightPicker.maxValue = 250
+
+        val genders = arrayOf("male","female")
+
+        genderPicker.minValue = 1
+        genderPicker.maxValue = 2
+        genderPicker.displayedValues = genders
+
+
         toolbar = findViewById(R.id.toolbar)
 
         toolbar.setNavigationIcon(R.drawable.back)
