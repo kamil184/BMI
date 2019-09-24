efeb1a62910506b264350b4c3aaf31475271e12f
kamil184 <45402327+kamil184@users.noreply.github.com>
2019-09-24 22:01:03 +0300

    some

--- app/src/main/java/com/bmi/BmiDetailsActivity.kt
+++ app/src/main/java/com/bmi/BmiDetailsActivity.kt
@@ -1,29 +1,23 @@
 package com.bmi
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import android.view.View
 import android.view.WindowManager
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class BmiDetailsActivity : AppCompatActivity() {
 
-    lateinit var toolbar: Toolbar
+    lateinit var back: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-             window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
-
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
 
         setContentView(R.layout.bmi_details)
-        toolbar = findViewById(R.id.toolbar)
 
-        toolbar.setNavigationIcon(R.drawable.back)
-
-        toolbar.setNavigationOnClickListener(object : View.OnClickListener {
-            override fun onClick(v: View) {
-                onBackPressed()
-            }
-        })
+        back = findViewById(R.id.back)
+        back.setOnClickListener {
+            onBackPressed()
+        }
     }
 }
