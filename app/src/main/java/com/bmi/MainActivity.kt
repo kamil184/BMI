73085ea108148b9f620676982c69b9a5fd6b44b3
kamil184 <45402327+kamil184@users.noreply.github.com>
2019-09-25 21:59:07 +0300

    please enter a name

--- app/src/main/java/com/bmi/MainActivity.kt
+++ app/src/main/java/com/bmi/MainActivity.kt
@@ -4,19 +4,23 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.WindowManager
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.NumberPicker
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatImageButton
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var name: TextInputEditText
+    lateinit var nameEditText: TextInputEditText
     lateinit var back: ImageView
     lateinit var calculate: AppCompatImageButton
     lateinit var weightPicker: NumberPicker
     lateinit var heightPicker: NumberPicker
     lateinit var genderPicker: NumberPicker
+    lateinit var linearLayout: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,11 +30,12 @@ class MainActivity : AppCompatActivity() {
         )
         setContentView(R.layout.activity_main)
 
-        name = findViewById(R.id.name)
+        nameEditText = findViewById(R.id.name)
         calculate = findViewById(R.id.calculate)
         weightPicker = findViewById(R.id.weight_picker)
         heightPicker = findViewById(R.id.height_picker)
         genderPicker = findViewById(R.id.gender_picker)
+        linearLayout = findViewById(R.id.linear)
 
         weightPicker.wrapSelectorWheel = false
         heightPicker.wrapSelectorWheel = false
@@ -54,12 +59,15 @@ class MainActivity : AppCompatActivity() {
         }
 
         calculate.setOnClickListener {
-            val intent = Intent(this@MainActivity,BmiDetailsActivity::class.java)
-            intent.putExtra("height",heightPicker.value)
-            intent.putExtra("weight",weightPicker.value)
-            intent.putExtra("gender",genderPicker.value)
-            intent.putExtra("name",name.text.toString())
-            startActivity(intent)
+            val name = nameEditText.text.toString()
+            if (name.length != 0) {
+                val intent = Intent(this@MainActivity, BmiDetailsActivity::class.java)
+                intent.putExtra("height", heightPicker.value)
+                intent.putExtra("weight", weightPicker.value)
+                intent.putExtra("gender", genderPicker.value)
+                intent.putExtra("name", name)
+                startActivity(intent)
+            }else {Snackbar.make(linearLayout, "Please enter a name", Snackbar.LENGTH_SHORT).show()}
         }
     }
 }
